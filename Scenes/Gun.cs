d1e908ab21cc0f36e417a5d216714ec363669a40
17steen <robinoger1207@gmail.com>
2021-10-28 23:30:30 +0200

    added gun sprite and make it follow the mouse

---
+++ Scenes/Gun.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class Gun : Node2D
+{
+    private Position2D pivot;
+    private float rotation;
+
+    // angle 
+    float distance;
+    public override void _Ready()
+    {
+        pivot = GetNode<Position2D>("../Pivot");
+        distance = this.Position.x;
+    }
+
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+
+        var mousePos = this.GetViewport().GetMousePosition();
+        var relativePlayerPos = pivot.GetGlobalTransformWithCanvas();
+        var vec = relativePlayerPos.origin;
+
+        rotation = mousePos.AngleToPoint(vec);
+
+        var position = new Vector2(distance, 0).Rotated(rotation);
+        this.Rotation = rotation;
+        Position = position;
+
+    }
+
+}
